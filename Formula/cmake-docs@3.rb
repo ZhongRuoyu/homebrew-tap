884197ec0ee11e752bbe91988a63dbfb06cd220a
Ruoyu Zhong <zhongruoyu@outlook.com>
2025-08-31 13:22:01 +0800

    cmake-docs@3 3.31.8 (new formula)

---
+++ Formula/cmake-docs@3.rb
@@ -0,0 +1,34 @@
+class CmakeDocsAT3 < Formula
+  desc "Documentation for CMake"
+  homepage "https://www.cmake.org/"
+  url "https://github.com/Kitware/CMake/releases/download/v3.31.8/cmake-3.31.8.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/cmake-3.31.8.tar.gz"
+  mirror "http://fresh-center.net/linux/misc/legacy/cmake-3.31.8.tar.gz"
+  sha256 "e3cde3ca83dc2d3212105326b8f1b565116be808394384007e7ef1c253af6caa"
+  license "BSD-3-Clause"
+  head "https://gitlab.kitware.com/cmake/cmake.git", branch: "master"
+
+  livecheck do
+    formula "zhongruoyu/tap/cmake@3"
+  end
+
+  depends_on "zhongruoyu/tap/cmake@3" => :build
+  depends_on "sphinx-doc" => :build
+
+  def install
+    args = %w[
+      -DCMAKE_DOC_DIR=share/doc/cmake
+      -DCMAKE_MAN_DIR=share/man
+      -DSPHINX_MAN=ON
+      -DSPHINX_HTML=ON
+    ]
+    system "cmake", "-S", "Utilities/Sphinx", "-B", "build", *args, *std_cmake_args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+  end
+
+  test do
+    assert_path_exists share/"doc/cmake/html"
+    assert_path_exists man
+  end
+end
